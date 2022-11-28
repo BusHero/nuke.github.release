a3cf2fbd25083cbe927a1c0e21be45b180c3ccac
Cervac Petru <petru.cervac@gmail.com>
2022-11-28 23:07:02 +0200

    ci: add milestone checking (#3)
    * ci: add milestone checking
    
    * ci: remove showing of the version
    
    * ci: rename the step where the milestone is shown
    
    * build: Create a new milestone
    
    * ci: update cache action to v3
    
    * build: add assertions
    
    * ci: finish milestone

--- build/Build.Release.cs
+++ build/Build.Release.cs
@@ -1,4 +1,7 @@
+using FluentAssertions.Execution;
 using Nuke.Common;
+using Nuke.Common.Git;
+using Nuke.Common.Tools.GitHub;
 using Nuke.Common.Tools.GitVersion;
 using Serilog;
 
@@ -7,8 +10,9 @@ partial class Build
 	[Parameter]
 	readonly bool Major;
 
-	[GitVersion]
-	GitVersion GitVersion;
+	[GitVersion] GitVersion GitVersion;
+
+	[GitRepository] GitRepository GitRepository;
 
 	string MajorMinorPatchVersion => Major ? $"{GitVersion.Major + 1}.0.0" : GitVersion.MajorMinorPatch;
 
@@ -16,5 +20,18 @@ partial class Build
 		.Executes(() =>
 		{
 			Log.Information("Version - {Version}", MajorMinorPatchVersion);
+		});
+
+	Target Milestone => _ => _
+		.Executes(async () =>
+		{
+			var milestoneTitle = $"v{MajorMinorPatchVersion}";
+			var milestone = await GitRepository.GetGitHubMilestone(milestoneTitle);
+
+			await GitRepository.TryCreateGitHubMilestone(milestoneTitle);
+			Log.Information("Milestone - '{Milestone}'", milestone);
+			Log.Information("Milestone Open issues - '{MilestoneOpenIssues}'", milestone?.OpenIssues);
+			Log.Information("Milestone Closed issues - '{MilestoneClosedIssues}'", milestone?.ClosedIssues);
+			Log.Information("Milestone State - '{MilestoneState}'", milestone?.State);
 		});
 }
