adc1ba4168d67a52763826e226753a0e42d9e67f
Cervac Petru <petru.cervac@gmail.com>
2022-11-29 22:56:25 +0200

    feat: add changelog (#5)
    * build: add changelog
    
    * ci: run changelog in pipeline
    
    * IDK

--- build/Build.Release.cs
+++ build/Build.Release.cs
@@ -1,9 +1,13 @@
-using FluentAssertions.Execution;
 using Nuke.Common;
 using Nuke.Common.Git;
+using Nuke.Common.IO;
 using Nuke.Common.Tools.GitHub;
 using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.ChangeLog.ChangelogTasks;
+using static Nuke.Common.Tools.Git.GitTasks;
+using Nuke.Common.Tools.Git;
 using Serilog;
+using System.IO;
 
 partial class Build
 {
@@ -13,6 +17,8 @@ partial class Build
 	[GitVersion] GitVersion GitVersion;
 
 	[GitRepository] GitRepository GitRepository;
+
+	const string MasterBranch = "main";
 
 	string MajorMinorPatchVersion => Major ? $"{GitVersion.Major + 1}.0.0" : GitVersion.MajorMinorPatch;
 
@@ -33,5 +39,24 @@ partial class Build
 			Log.Information("Milestone Open issues - '{MilestoneOpenIssues}'", milestone?.OpenIssues);
 			Log.Information("Milestone Closed issues - '{MilestoneClosedIssues}'", milestone?.ClosedIssues);
 			Log.Information("Milestone State - '{MilestoneState}'", milestone?.State);
+		});
+
+	AbsolutePath ChangelogFile => RootDirectory / "CHANGELOG.md";
+
+	Target EnsureChangelogFile => _ => _
+		.Unlisted()
+		.Executes(() =>
+		{
+			using var _ = File.CreateText(ChangelogFile);
+		});
+
+	Target Release => _ => _
+		.DependsOn(EnsureChangelogFile)
+		.Executes(() =>
+		{
+			Git($"checkout {MasterBranch}");
+			Git($"merge --no-ff --no-edit {GitRepository.Branch}");
+			Git($"tag {MajorMinorPatchVersion}");
+			Git($"push origin {MasterBranch} {MajorMinorPatchVersion}");
 		});
 }
