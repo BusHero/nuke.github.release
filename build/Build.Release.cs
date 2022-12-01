403f985b829e689f215db32d186bef780ca7cdc5
bus1hero <petru.cervac@gmail.com>
2022-12-01 21:37:08 +0200

    chore: fix error with GitHubToken

--- build/Build.Release.cs
+++ build/Build.Release.cs
@@ -20,7 +20,7 @@ partial class Build
 
 	[GitRepository] GitRepository Repository;
 
-	[Parameter, Secret] readonly string GitHubToken = GitHubActions.Instance.Token;
+	[Parameter, Secret] readonly string GitHubToken = GitHubActions.Instance?.Token;
 
 	GitHubActions GitHubActions => GitHubActions.Instance;
 
