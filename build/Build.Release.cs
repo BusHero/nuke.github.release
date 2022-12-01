5838395c6aac7ef3bc7ff17d2202e4cc54bacba1
Cervac Petru <petru.cervac@gmail.com>
2022-12-01 19:34:54 +0200

    fix release (#9)
    * fix release
    
    * update pipeline
    
    * update pipeline
    
    * asd
    
    * asd

--- build/Build.Release.cs
+++ build/Build.Release.cs
@@ -7,14 +7,9 @@ using static Nuke.Common.Tools.GitHub.GitHubTasks;
 using Serilog;
 using System.IO;
 using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.Utilities;
+using System.Net.Http.Headers;
+using Octokit;
 
-[GitHubActions(
-	"foo",
-	GitHubActionsImage.UbuntuLatest,
-	On = new[] { GitHubActionsTrigger.Push },
-	InvokedTargets = new[] { nameof(Compile) },
-	EnableGitHubToken = true)]
 partial class Build
 {
 	[Parameter]
@@ -22,7 +17,7 @@ partial class Build
 
 	[GitVersion] GitVersion GitVersion;
 
-	[GitRepository] GitRepository GitRepository;
+	[GitRepository] GitRepository Repository;
 
 	GitHubActions GitHubActions => GitHubActions.Instance;
 
@@ -40,9 +35,9 @@ partial class Build
 		.Executes(async () =>
 		{
 			var milestoneTitle = $"v{MajorMinorPatchVersion}";
-			var milestone = await GitRepository.GetGitHubMilestone(milestoneTitle);
+			var milestone = await Repository.GetGitHubMilestone(milestoneTitle);
 
-			await GitRepository.TryCreateGitHubMilestone(milestoneTitle);
+			await Repository.TryCreateGitHubMilestone(milestoneTitle);
 			Log.Information("Milestone - '{Milestone}'", milestone);
 			Log.Information("Milestone Open issues - '{MilestoneOpenIssues}'", milestone?.OpenIssues);
 			Log.Information("Milestone Closed issues - '{MilestoneClosedIssues}'", milestone?.ClosedIssues);
@@ -58,9 +53,67 @@ partial class Build
 			using var _ = File.CreateText(ChangelogFile);
 		});
 
-	Target Release => _ => _
+
+	Target ShowRepositoryInfo => _ => _
+		.Executes(() =>
+		{
+			Log.Information("Commit = {Value}", Repository.Commit);
+			Log.Information("Branch = {Value}", Repository.Branch);
+			Log.Information("Tags = {Value}", Repository.Tags);
+
+			Log.Information("main branch = {Value}", Repository.IsOnMainBranch());
+			Log.Information("main/master branch = {Value}", Repository.IsOnMainOrMasterBranch());
+			Log.Information("release/* branch = {Value}", Repository.IsOnReleaseBranch());
+			Log.Information("hotfix/* branch = {Value}", Repository.IsOnHotfixBranch());
+
+			Log.Information("Https URL = {Value}", Repository.HttpsUrl);
+			Log.Information("SSH URL = {Value}", Repository.SshUrl);
+		});
+
+	Target ShowActionInfo => _ => _
 		.Executes(() =>
 		{
 			Log.Information("Token - {Token}", GitHubActions.Token);
+			Log.Information("Action - {Action}", GitHubActions.Action);
+			Log.Information("Actor - {Actor}", GitHubActions.Actor);
+			Log.Information("BaseRef - {BaseRef}", GitHubActions.BaseRef);
+			Log.Information("EventName - {EventName}", GitHubActions.EventName);
+			Log.Information("EventPath - {EventPath}", GitHubActions.EventPath);
+			Log.Information("GitHubEvent - {GitHubEvent}", GitHubActions.GitHubEvent);
+			Log.Information("HeadRef - {HeadRef}", GitHubActions.HeadRef);
+			Log.Information("Home - {Home}", GitHubActions.Home);
+			Log.Information("IsPullRequest - {IsPullRequest}", GitHubActions.IsPullRequest);
+			Log.Information("Job - {Job}", GitHubActions.Job);
+			Log.Information("PullRequestAction - {PullRequestAction}", GitHubActions.PullRequestAction);
+			Log.Information("PullRequestNumber - {PullRequestNumber}", GitHubActions.PullRequestNumber);
+			Log.Information("Ref - {Ref}", GitHubActions.Ref);
+			Log.Information("Repository - {Repository}", GitHubActions.Repository);
+			Log.Information("RepositoryOwner - {RepositoryOwner}", GitHubActions.RepositoryOwner);
+			Log.Information("RunId - {RunId}", GitHubActions.RunId);
+			Log.Information("RunNumber - {RunNumber}", GitHubActions.RunNumber);
+			Log.Information("ServerUrl - {ServerUrl}", GitHubActions.ServerUrl);
+			Log.Information("Sha - {Sha}", GitHubActions.Sha);
+			Log.Information("Token - {Token}", GitHubActions.Token);
+			Log.Information("Workflow - {Workflow}", GitHubActions.Workflow);
+			Log.Information("Workspace - {Workspace}", GitHubActions.Workspace);
+		});
+
+	Target Release => _ => _
+		.Executes(() =>
+		{
+			var credentials = new Credentials(GitHubActions.Token);
+			GitHubTasks.GitHubClient = new GitHubClient(
+				new Octokit.ProductHeaderValue(nameof(NukeBuild)),
+				new Octokit.Internal.InMemoryCredentialStore(credentials));
+			var release = new NewRelease(MajorMinorPatchVersion)
+			{
+				Name = $"Release {MajorMinorPatchVersion}",
+				Prerelease = true,
+				Body = "Some body here and there"
+			};
+			var createdRelease = GitHubTasks.GitHubClient.Repository.Release.Create(
+				GitHubActions.RepositoryOwner,
+				"nuke.github.release",
+				release).Result;
 		});
 }
