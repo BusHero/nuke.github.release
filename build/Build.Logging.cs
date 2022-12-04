947f135540237d292f8ae55ec0cc0e11d17ebea2
bus1hero <petru.cervac@gmail.com>
2022-12-05 00:03:59 +0200

    feat: add pester to nuke

--- build/Build.Logging.cs
+++ build/Build.Logging.cs
@@ -1,9 +1,42 @@
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Serilog;
+using static Nuke.Common.Tools.PowerShell.PowerShellTasks;
+using Nuke.Common.Tools.PowerShell;
+using Nuke.Common.Tooling;
 
 partial class Build
 {
+	Target ExecuteTests => _ => _
+		.Executes(() =>
+		{
+			PowerShell(_ => _
+				.SetFile(RootDirectory / "runners" / "tests.runner.ps1")
+				.SetNoProfile(true)
+				.SetNoLogo(true)
+				.SetProcessToolPath("pwsh"));
+		});
+
+	Target ExecuteTestsFixLogging => _ => _
+		.Executes(() =>
+		{
+			var currentLogger = Log.Logger;
+			Log.Logger = new LoggerConfiguration()
+				.WriteTo.Console(
+					outputTemplate: "{Message:l}{NewLine}{Exception}",
+					applyThemeToRedirectedOutput: true)
+				.ConfigureInMemory(this)
+				.ConfigureFiles(this)
+				.ConfigureLevel()
+				.ConfigureFilter()
+				.CreateLogger();
+			PowerShell(_ => _
+				.SetFile(RootDirectory / "runners" / "tests.runner.ps1")
+				.SetNoProfile(true)
+				.SetNoLogo(true)
+				.SetProcessToolPath("pwsh"));
+		});
+
 	Target Logs => _ => _
 		.Executes(() =>
 		{
