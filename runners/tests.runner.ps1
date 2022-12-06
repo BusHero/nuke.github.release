975449000a12885f177194e3973874b2352f08fd
bus1hero <petru.cervac@gmail.com>
2022-12-06 23:46:37 +0200

    asd

--- runners/tests.runner.ps1
+++ runners/tests.runner.ps1
@@ -1,12 +1,13 @@
-Import-Module Pester
-$version =
+# Import-Module Pester
+# $version =
 
-Write-Warning "Pester Version: $(Get-Module Pester | Select-Object -ExpandProperty Version)"
-Write-Warning "Pester Path: $(Get-Module Pester | Select-Object -ExpandProperty Path)"
+# Write-Warning "Pester Version: $(Get-Module Pester | Select-Object -ExpandProperty Version)"
+# Write-Warning "Pester Path: $(Get-Module Pester | Select-Object -ExpandProperty Path)"
 
-[PesterConfiguration] $configuration = New-PesterConfiguration
-$configuration.Run.Path = "${PSScriptRoot}\..\tests"
-$configuration.Run.Exit = $true
-$configuration.Output.CIFormat = 'GithubActions'
-$psEditor = 'fuck'
-Invoke-Pester -Configuration $configuration
+# [PesterConfiguration] $configuration = New-PesterConfiguration
+# $configuration.Run.Path = "${PSScriptRoot}\..\tests"
+# $configuration.Run.Exit = $true
+# $configuration.Output.CIFormat = 'GithubActions'
+# $psEditor = 'fuck'
+# Invoke-Pester -Configuration $configuration
+exit 1
